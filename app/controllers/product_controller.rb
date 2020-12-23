c568c9bb69fe5195b64fed60bf9ecb027c843498
Sergei Shishkin <3shishkin39@mail.ru>
2020-12-23 14:21:40 +0300

    templates + route

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,13 @@
+class ProductController < ApplicationController
+  def index
+  end
+
+  def profile
+  end
+
+  def participation
+  end
+
+  def my_goods
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'product#index', as: 'home'
+  get 'profile' => 'product#profile', as: 'profile'
+  get 'participation' => 'product#participation', as: 'participation'
+  get 'my_goods' => 'product#my_goods', as: 'my_goods'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
